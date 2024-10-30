ce04af01ff8a5be8e4daf80bc3151f39e6f8c8c5
IniArthika <arthikasalsabila@gmail.com>
2024-10-30 12:42:45 +0700

    fix

--- form.php
+++ form.php
@@ -3,28 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>form</title>
+    <title>Input Data Product</title>
 </head>
 <body>
-    
-    <form action="data.php" enctype="multipart/form-data" method="POST">
-        <!-- input nama : single line text input -->
-        Nama : <input type="text" name="nama" value=""></br>
-        NISN : <input type="number" name="nisn" id=""></br>
-        NIS  : <input type="text" name="nis" id=""></br>
-        Jurusan : 
-        <select name="jurusan" id="">
-            <option value="0">-- pilih jurusan --</option>
-            <option value="1">PPLG</option>
-            <option value="2">DKV</option>
-            <option value="3">BDP</option>
-            <option value="4">AKL</option>
-            <option value="5">TJKT</option>
-            <option value="6">MPLB</option>
-        </select></br>
+    <form action="data.php" enctype="multipart/form-data" method="POST" >
+        <!-- input nama : single line text input  -->
         
+        Nama : <input type="text" name="nama" value="" /><br/>
+        Deskripsi : <textarea name="deskripsi" id="" cols="30" rows="10"></textarea>
+        <br/>
+        Harga : <input type="number" name="harga" value="" /><br/>
+        Kategori_id : 
+        <select name="kategori_id" id="">
+            <option value="0">--Pilih Kategori Id--</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+        </select> <br/>
+        Foto : <input type="file" name="foto" id=""><br/>
+        
+        <input type="submit" value="Input Data" name="inputData"> 
+        <!-- <button type="submit">Inputkan</button> -->
+
+
     </form>
-
-
+    
 </body>
 </html>
