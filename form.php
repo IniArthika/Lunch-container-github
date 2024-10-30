572d5b8d952956e28f9e80edc465e63799afc7b4
IniArthika <arthikasalsabila@gmail.com>
2024-10-30 14:17:52 +0700

    baru

--- form.php
+++ form.php
@@ -6,26 +6,72 @@
     <title>Input Data Product</title>
 </head>
 <body>
+    
+
+
+
     <form action="data.php" enctype="multipart/form-data" method="POST" >
         <!-- input nama : single line text input  -->
-        
-        Nama : <input type="text" name="nama" value="" /><br/>
-        Deskripsi : <textarea name="deskripsi" id="" cols="30" rows="10"></textarea>
-        <br/>
-        Harga : <input type="number" name="harga" value="" /><br/>
-        Kategori_id : 
-        <select name="kategori_id" id="">
-            <option value="0">--Pilih Kategori Id--</option>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-        </select> <br/>
-        Foto : <input type="file" name="foto" id=""><br/>
-        
-        <input type="submit" value="Input Data" name="inputData"> 
-        <!-- <button type="submit">Inputkan</button> -->
+        <center>
+            <h4>Form Input Data Barang</h4>
+                <table>
+                    <tr>
+                        <td>
+                            Nama Barang
+                        </td>
+                        <td>
+                            <input type="text" placeholder="Nama Barang" name="nama" value="" ></br>
+                        </td>
+                    </tr>
 
+                    <tr>
+                        <td>
+                            Deskripsi
+                        </td>
+                        <td>
+                            <textarea type="text" placeholder="Deskripsi" name="deskripsi" id="" cols="30" rows="10"></textarea><br/>
+                        </td>
+                    </tr>
 
+                    <tr>
+                        <td>
+                            Harga
+                        </td>
+                        <td>
+                            <input type="number" placeholder="Harga" name="harga" value="" ><br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Kategori_id
+                        </td>
+                        <td>
+                            <select name="kategori_id" id="">
+                                <option value="0">--Pilih Kategori Id--</option>
+                                <option value="1">1</option>
+                                <option value="2">2</option>
+                                <option value="3">3</option>
+                            </select> <br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                            Foto
+                        </td>
+                        <td>
+                        <input type="file" name="foto" placeholder="Foto" id=""><br/>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>
+                        <input type="submit" value="Input Data" name="inputData">
+                        </td>
+                    </tr>
+                </table>
+        </center>
     </form>
     
 </body>
