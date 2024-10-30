406032c5598a1460cb51ad48be8309437182a0ac
IniArthika <arthikasalsabila@gmail.com>
2024-10-30 14:28:51 +0700

    fix

--- data.php
+++ data.php
@@ -3,22 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Data Barang</title>
 </head>
 <body>
     
     
     <?php
     $nama = $_POST ['nama'];
-    $deskripsi = $_POST ['deskripsi'];
-    $harga = $_POST ['harga'];
-    $kategori_id = $_POST ['kategori_id'];
-    $foto = $_POST ['foto'];
+        $deskripsi = $_POST ['deskripsi'];
+            $harga = $_POST ['harga'];
+                $kategori_id = $_POST ['kategori_id'];
+                    $foto = $_POST ['foto'];
 
-    echo "<p>nama $nama</p>" ;
-    echo "<p>deskripsi $deskripsi</p>" ;
-    echo "<p>harga $harga</p>" ;
-    echo "<p>kategori_id $kategori_id</p>" ;
+                    echo "<p>nama $nama</p>" ;
+                echo "<p>deskripsi $deskripsi</p>" ;
+            echo "<p>harga $harga</p>" ;
+        echo "<p>kategori_id $kategori_id</p>" ;
     echo "<p>foto $foto</p>" ;
     ?>
    
